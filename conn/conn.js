177506c4b6e890f434bad8870adbb745d8e994a0
Leandro da Costa Romualdo <89050565+LeandroKosta@users.noreply.github.com>
2021-11-25 00:27:17 -0300

    alteração

--- conn/conn.js
+++ conn/conn.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
-const Conn = (user, pass, data) => {
+const Conn = (url, user, pass, data) => {
   mongoose
-    .connect(
-      `mongodb://${user}:${pass}@projetolistadetarefa-shard-00-00.p1ovi.mongodb.net:27017,projetolistadetarefa-shard-00-01.p1ovi.mongodb.net:27017,projetolistadetarefa-shard-00-02.p1ovi.mongodb.net:27017/${data}?ssl=true&replicaSet=atlas-y60afp-shard-0&authSource=admin&retryWrites=true&w=majority`
+    .connect(`${url}/${data}`,{
+      user: user,
+      pass: pass,
+    }      
     )
     .then(() => {
       console.log("Conexão com o MongoDB executada com sucesso!");
@@ -14,3 +16,5 @@ const Conn = (user, pass, data) => {
 };
 
 module.exports = Conn;
+
+//`mongodb://${user}:${pass}@projetolistadetarefa-shard-00-00.p1ovi.mongodb.net:27017,projetolistadetarefa-shard-00-01.p1ovi.mongodb.net:27017,projetolistadetarefa-shard-00-02.p1ovi.mongodb.net:27017/${data}?ssl=true&replicaSet=atlas-y60afp-shard-0&authSource=admin&retryWrites=true&w=majority`
