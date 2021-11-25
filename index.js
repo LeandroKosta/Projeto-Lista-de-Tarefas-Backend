bbe2d6d7d0ff6f21627c120a9edc88108fb0b54c
Leandro da Costa Romualdo <89050565+LeandroKosta@users.noreply.github.com>
2021-11-25 00:30:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ app.use(express.json());
 
 app.use("/todolist", TodoListRouter);
 
+const db_url = process.env.DB_URL;
 const db_user = process.env.DB_USER;
 const db_pass = process.env.DB_PASS;
 const db_data = process.env.DB_DATA;
-Conn(db_user, db_pass, db_data);
+Conn(db_url, db_user, db_pass, db_data);
 
 const port = 3001;
 app.listen(process.env.PORT || port, () =>
