2532ecb4ee99d43919fc82bff7cd5e0effc8f936
Leandro da Costa Romualdo <89050565+LeandroKosta@users.noreply.github.com>
2021-11-24 23:50:19 -0300

    alteração

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const TodoListRouter = require("./routes/todolist.routes");
 const Conn = require("./conn/conn");
 const app = express();
 
-
 app.use(express.json());
 //app.use(cors());
 
@@ -17,7 +16,7 @@ app.use("/todolist", TodoListRouter);
 const db_user = process.env.DB_USER;
 const db_pass = process.env.DB_PASS;
 const db_data = process.env.DB_DATA;
-Conn(db_user,db_pass, db_data);
+Conn(db_user, db_pass, db_data);
 
 const port = 3001;
 app.listen(process.env.PORT || port, () =>
